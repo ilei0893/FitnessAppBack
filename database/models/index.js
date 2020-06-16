5ad53bd40d08fec5b77d0e3d54a2e6c8d7de2df1
BuildTools <justinkrasner98@gmail.com>
2020-06-15 22:21:40 -0400

    filled out database/models/index.js

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,22 @@
 // Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
 
-const Food = require("./food");
+const Account = require("./account");
+const Entries = require("./entries");
+const FoodEntry = require("./foodEntry");
+const ExerciseEntry = require("./exerciseEntry");
+
+Account.hasMany(Entries);
+
+Entries.hasMany(FoodEntry);
+Entries.hasMany(ExerciseEntry);
+
+FoodEntry.belongsTo(Account);
+ExerciseEntry.belongsTo(Account);
+
 
 module.exports = {
-  Food,
+  Account,
+  Entries,
+  FoodEntry,
+  ExercsieEntry  
 };
